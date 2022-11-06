46f37cd47fc7eccc259306d38996bfe330d6b2cc
Es-ashraf <islam.ashraf@dafa.sa>
2022-11-06 14:47:43 +0200

    reservation

--- src/modules/books/views/index.vue
+++ src/modules/books/views/index.vue
@@ -19,7 +19,26 @@ export default {
         { label: 'تكلفة الكشف', key: 'price', class: 'text-right' },
         { label: 'تاريخ الزيارة', key: 'time.date', class: 'text-right' },
         { label: 'التشخيص', key: 'diagnosis', type: 'status', class: 'text-right' },
-        { label: 'التحكم', key: 'actions', class: 'text-right', type: 'actions' },
+        {
+          label: 'التحكم',
+          key: 'actions',
+          class: 'text-right',
+          type: 'actions',
+          actions: [
+            {
+              text: 'Delete',
+              icon: 'ri-delete-bin-line',
+              color: 'danger',
+              url: 'merchant/products',
+              titleHeader: 'منتج',
+              question: 'هل متأكد أنك تريد مسح هذا المنتج',
+              textContent: 'name',
+              textDeleteButton: 'بالتأكيد, مسح',
+              textCancelButton: 'الغاء',
+              showAlert: true
+            }
+          ]
+        },
         { label: 'Created At', key: 'created_at', class: 'text-right' }
       ]
     }
